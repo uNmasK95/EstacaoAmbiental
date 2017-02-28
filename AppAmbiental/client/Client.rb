dc989c965562d2e2a6018e890462e15d8c24372b
misticstrong <pmbdv@hotmail.com>
2017-02-28 17:16:34 +0000

    Modulo da Temperatura e Ruido feito

--- AppAmbiental/client/Client.rb
+++ AppAmbiental/client/Client.rb
@@ -1,18 +1,42 @@
 require 'socket'
+require_relative "XdkSensor.rb"
 
 class Client
+
+  include XdkSensor
 
   def initialize( hostname, port)
     @hostname = hostname
     @port = port
+    @latitude = rand(-90.0..90.0)
+    @longitude = rand(-180.0..180)
+    super()
   end
 
   def start
-    s = TCPSocket.open(@hostname, @port)
 
-    while line = s.gets   # Read lines from the socket
-      puts line.chop      # And print with platform line terminator
-    end
+    socket = TCPSocket.open(@hostname, @port)
+
+    # Thread Ruido  Colocar uma Thread para o ruido com => valor;Timestamp;gps;
+    #Thread.new{
+     # loop do
+        #puts"#{self.getTemperatura}"
+      #  puts("Ruido\n")
+      #  sleep(1)
+      #end
+   # }
+
+    # Thread Temperatura   Colocar uma Thread para a Temperatura com => valor;Timestamp;gps;
+    Thread.new {
+      loop do
+        puts"asas #{getTemperatura}"
+        puts("Temperatura\n")
+        #time.now
+        sleep(1)
+      end
+    }.join
+
+    #O gps tem latitude e longitude exemplo => 99.70:-73.9893;
 
   end
 end
--- AppAmbiental/server/server.rb
+++ AppAmbiental/server/server.rb
@@ -20,8 +20,7 @@ class Server
         end
 
       end
-    }
-
+    }.join
   end
 
 end
