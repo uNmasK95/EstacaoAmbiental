f505091732d3787640b21bdccd309d17440710a8
Rui Freitas <rui4825@gmail.com>
2017-03-10 13:48:55 +0000

    alterei a passagem do id para os argumentos do programa

--- AppAmbiental/client/Client.rb
+++ AppAmbiental/client/Client.rb
@@ -20,36 +20,31 @@ class Client
 
     socket.puts("#{@id}")
 
-    puts socket.gets
+    puts "Client ID: #{socket.gets}"
 
-    # Thread Ruido  Colocar uma Thread para o ruido com => valor;Timestamp;gps;
     Thread.new{
       loop do
-        puts "ruido"
         socket.write("ruido;#{getRuido};#{Time.now.to_i};#{@latitude};#{@longitude}\n")
         sleep(1)
       end
     }
 
-    # Thread Temperatura   Colocar uma Thread para a Temperatura com => valor;Timestamp;gps;
     Thread.new {
       loop do
-        puts "temperatura"
         socket.write("temperatura;#{getTemperatura};#{Time.now.to_i};#{@latitude};#{@longitude}\n")
         sleep(30)
       end
     }.join
-
-    #O gps tem latitude e longitude exemplo => 99.70:-73.9893;
 
   end
 
 end
 
 
-puts "Insira o ID:"
-b = gets.chomp.to_i
 
-c = Client.new 'localhost', 12346, b
+if __FILE__ == $PROGRAM_NAME
 
-c.start
+  c = Client.new 'localhost', 12346, ARGV[0]
+  c.start
+
+end
